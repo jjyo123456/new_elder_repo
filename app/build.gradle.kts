2e0178e35b71d7a6d465bf00470637246ea1b1a8
jjyo123456 <jasjyotsingh11@gmail.com>
2025-03-11 19:10:39 +0530

    third

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,47 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.firebase.firestore)
+
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    implementation("com.google.firebase:firebase-auth:22.3.1") // Downgraded version
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+
+
+
+    implementation (libs.google.webrtc)
+
+    implementation ("com.mesibo.api:calls:2.7.18+")
+
+    implementation ("com.google.firebase:firebase-database:20.0.2")
+
+
+
+
+
+
+    implementation ("com.google.mediapipe:tasks-vision:latest.release")
+
+    implementation ("androidx.camera:camera-core:1.3.0")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
+
+
+
+
+    implementation (libs.mediapipe.tasks.vision)
+
 }
